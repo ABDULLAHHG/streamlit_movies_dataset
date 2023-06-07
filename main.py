8a0715fde9dc7b73b6bc480ace83fd09ee9b1cbe
Aboud <gh.abd.com.207@gmail.com>
2023-06-07 22:03:09 +0300

    edit on distroplot

--- main.py
+++ main.py
@@ -9,8 +9,13 @@ df = df.dropna()
 df['date_x'] = pd.to_datetime(df['date_x']) 
 df['year'] = df['date_x'].dt.year
 df = df.set_index('date_x')
-st.dataframe(df.sort_values(by = 'budget_x'))
+st.dataframe(df)
+st.dataframe(df[df['genre'] == 'Action'])
+
  
+
+colors = ['#7c90db', '#92a8d1', '#a5c4e1', '#f7cac9', '#fcbad3', '#e05b6f', '#f8b195', '#f5b971', '#f9c74f', '#ee6c4d', '#c94c4c', '#589a8e', '#a381b5', '#f8961e', '#4f5d75', '#6b5b95', '#9b59b6', '#b5e7a0', '#a2b9bc', '#b2ad7f', '#679436', '#878f99', '#c7b8ea', '#6f9fd8', '#d64161', '#f3722c', '#f9a828', '#ff7b25', '#7f7f7f']
+
 
 def scatter(df):
     df =df.sort_values(by = 'budget_x')
@@ -45,21 +50,37 @@ def scatter(df):
 def distribution (df):
     column = st.selectbox('select column' , [i for i in df.columns if i != 'names'])
     unique_count = st.select_slider('select number of unique count',[i for i in range(len(df[column].value_counts())+1)] , len(df[column].value_counts().index)//5)
-    fig = make_subplots(rows = 1 , cols = 2 , subplot_titles=('countplot','percentage'))
+    fig = make_subplots(rows = 1 , cols = 2 , subplot_titles=('countplot','percentage'), specs=[[{"type": "xy"}, {'type': 'domain'}]])
     x = df[column].value_counts().values
     y = df[column].value_counts().index
 
-    fig.add_trace(go.Pie(values = x[0:unique_count] ,
-                    labels=y[0:unique_count],
-                    textposition='auto',
-                    hoverinfo='label',
-                    
-                    ))
+
     
 
     fig.add_trace(go.Bar(x = x[0:unique_count],
                          y = y[0:unique_count],
-                         ))
+                         textposition='auto',
+                         marker = dict(
+                                        color = colors, 
+                                        line = dict(color = 'black',width = 0.1))
+                         ),row = 1 ,col = 1)
+    
+    fig.add_trace(go.Pie(values = x[0:unique_count] ,
+                    labels=y[0:unique_count],       
+                    textposition='auto',
+                    hoverinfo='label',
+                    
+                    ),row = 1 , col = 2)
+    
+
+    fig.update_layout(
+        title = {'text' : f'Distribution of the {column}',
+                 'y' : 0.9,
+                 'x' : 0.5,
+                 'xanchor' : 'center',
+                  'yanchor' : 'top'},
+                  template = 'plotly_dark')
+    st.plotly_chart(fig)
     st.plotly_chart(fig)
     st.text( df[column].value_counts().index[:unique_count])
 
